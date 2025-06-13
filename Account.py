9c7f1ba113faea76e5e939cba025723d1fda6ae2
lordz <lordzierwin17@gmail.com>
2025-06-13 14:21:24 +0800

    Updated

---
+++ Account.py
@@ -0,0 +1,5 @@
+class accDetails:
+    Name = "Pedro Gil"
+    BankAccNum = '1123456780'
+    PIN = '103023'
+
---
+++ main.py
@@ -0,0 +1,91 @@
+import tkinter
+import tkinter as gui
+from Account import accDetails
+
+root = gui.Tk()
+root.geometry("400x500")
+
+root.columnconfigure(0, weight=1)
+
+
+###---Variables---###
+
+accNum = gui.StringVar()
+Pin = gui.StringVar()
+
+###---Functions---###
+
+def login():
+    if accNum.get() or Pin.get():
+        if accNum.get() == accDetails.BankAccNum and Pin.get() == accDetails.PIN:
+            print(f"Welcome, {accDetails.Name}!")
+        else:
+            print("Incorrect Account Number or PIN")
+    else:
+        print("Please enter a value")
+
+
+
+###---LoginGUI---###
+topPad = gui.Label(root,
+                      text="",
+                      font=('Poppins', 2, 'bold'),
+                      pady= 45
+
+                      )
+
+userLabel = gui.Label(root,
+                      text="Account Number:",
+                      font=('Poppins', 13, 'bold'),
+                      pady= 7
+                      )
+passLabel = gui.Label(root,
+                      text="PIN:",
+                      font=('Poppins', 13, 'bold'),
+                      pady= 6
+                      )
+spacing = gui.Label(root,
+                      text="",
+                      font=('Poppins', 2, 'bold')
+                      )
+
+user = gui.Entry(root,
+                 textvariable=accNum,
+                 font=('Poppins', 12),
+                 justify='center',
+                 relief='flat',
+                 highlightbackground='#c7c9c8',
+                 highlightcolor='#0ec93a',
+                 highlightthickness=2
+                 )
+pw = gui.Entry(root,
+               textvariable=Pin,
+               font=('Poppins', 12),
+               justify='center',
+               relief='flat',
+               show='*',
+               highlightbackground='#c7c9c8',
+               highlightcolor='#0ec93a',
+               highlightthickness=2
+               )
+
+login = gui.Button(root,
+                   text='Proceed',
+                   command=login,
+                   relief="flat",
+                   bg='#0ec93a',
+                   font=('Poppins',12,'bold'),
+                   width=13,
+                   fg= 'white',
+                   activebackground='white',
+                   activeforeground='#0ec93a',
+                   )
+topPad.grid(row=0,column=0)
+userLabel.grid(row=1, column=0)
+passLabel.grid(row=3,column=0)
+user.grid(row=2, column=0)
+pw.grid(row= 4, column=0)
+spacing.grid(row=5,column=0)
+login.grid(row=6,column=0)
+
+root.mainloop()
